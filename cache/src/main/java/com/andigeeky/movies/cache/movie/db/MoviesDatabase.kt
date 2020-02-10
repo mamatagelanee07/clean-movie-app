918182a32570d05bf0fbb987ffb54f43c7979c0f
mamatagelanee07 <gelaneem@gmail.com>
2020-02-10 22:57:17 +0000

    [Refactor] Removed instance creation of Database

--- cache/src/main/java/com/andigeeky/movies/cache/movie/db/MoviesDatabase.kt
+++ cache/src/main/java/com/andigeeky/movies/cache/movie/db/MoviesDatabase.kt
@@ -1,9 +1,7 @@
 package com.andigeeky.movies.cache.movie.db
 
 
-import android.content.Context
 import androidx.room.Database
-import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.andigeeky.movies.cache.movie.popular.CachedPopularMoviesDao
 import com.andigeeky.movies.cache.movie.popular.model.CachedMovie
@@ -20,26 +18,7 @@ abstract class MoviesDatabase @Inject constructor() : RoomDatabase() {
 
     abstract fun cachedPopularMoviesDao(): CachedPopularMoviesDao
 
-    private var database: MoviesDatabase? = null
-
-    private val sLock = Any()
-
-    fun getInstance(context: Context): MoviesDatabase {
-        if (database == null) {
-            synchronized(sLock) {
-                if (database == null) {
-                    database = Room.databaseBuilder(context.applicationContext,
-                            MoviesDatabase::class.java,NAME)
-                            .build()
-                }
-                return database!!
-            }
-        }
-        return database!!
-    }
-
     companion object{
         const val NAME = "movies.db"
     }
-
 }
